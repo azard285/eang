2d8fd0e8e9af52ce2b98d082a628bc6a178f99fc
azard285 <pavlenkopasa382@gmail.com>
2025-05-25 20:49:54 +0700

    finish hw8

--- hw7-8/keylist.erl
+++ hw7-8/keylist.erl
@@ -1,19 +1,102 @@
 -module(keylist).
--export([start_link/1, init/1, loop/0]).
+-author("Pavlenko").
+-description("Модуль для хранения списка ключ-значение с комментариями").
+-include("keylist.hrl").
 
+-export([
+    start_link/1,
+    init/1,
+    loop/1,
+    add/4,
+    is_member/2,
+    take/2,
+    find/2,
+    delete/2
+]).
+
+-type key() :: any().
+-type value() :: any().
+-type comment() :: any().
+-type element() :: {key(), value(), comment()}.
+
+%% @doc Запускает процесс keylist с именем.
+-spec start_link(atom()) -> {ok, pid()}.
 start_link(Name) ->
     Pid = spawn_link(?MODULE, init, [Name]),
     {ok, Pid}.
 
+%% @doc Инициализирует процесс keylist.
+-spec init(atom()) -> no_return().
 init(Name) ->
     register(Name, self()),
-    loop().
+    loop([]).
 
-loop() ->
+%% @doc Клиентская функция для добавления элемента.
+-spec add(atom(), key(), value(), comment()) -> ok.
+add(Name, Key, Value, Comment) ->
+    Name ! {self(), add, Key, Value, Comment},
+    ok.
+
+%% @doc Клиентская функция для проверки наличия ключа.
+-spec is_member(atom(), key()) -> ok.
+is_member(Name, Key) ->
+    Name ! {self(), is_member, Key},
+    ok.
+
+%% @doc Клиентская функция для извлечения элемента.
+-spec take(atom(), key()) -> ok.
+take(Name, Key) ->
+    Name ! {self(), take, Key},
+    ok.
+
+%% @doc Клиентская функция для поиска элемента.
+-spec find(atom(), key()) -> ok.
+find(Name, Key) ->
+    Name ! {self(), find, Key},
+    ok.
+
+%% @doc Клиентская функция для удаления элемента.
+-spec delete(atom(), key()) -> ok.
+delete(Name, Key) ->
+    Name ! {self(), delete, Key},
+    ok.
+
+%% @doc Основной цикл процесса keylist.
+-spec loop(list(element())) -> no_return().
+loop(List) ->
     receive
         stop ->
             ok;
 
-        _ ->
-            loop()
+        {From, add, Key, Value, Comment} ->
+            From ! ok,
+            loop([{Key, Value, Comment} | List]);
+
+        {From, is_member, Key} ->
+            From ! {ok, lists:keymember(Key, 1, List)},
+            loop(List);
+
+        {From, take, Key} ->
+            case lists:keytake(Key, 1, List) of
+                {value, Element, NewList} ->
+                    From ! {ok, Element},
+                    loop(NewList);
+                false ->
+                    From ! {ok, not_found},
+                    loop(List)
+            end;
+
+        {From, find, Key} ->
+            case lists:keyfind(Key, 1, List) of
+                Element when is_tuple(Element) ->
+                    From ! {ok, Element};
+                false ->
+                    From ! {ok, not_found}
+            end,
+            loop(List);
+
+        {From, delete, Key} ->
+            NewList = lists:keydelete(Key, 1, List),
+            From ! ok,
+            loop(NewList)
     end.
--- hw7-8/keylist_manager.erl
+++ hw7-8/keylist_manager.erl
@@ -1,20 +1,29 @@
 -module(keylist_manager).
+-author("Pavlenko").
+-description("Модуль для хранения списка ключ-значение с комментариями").
 -include("keylist.hrl").
 
 -export([start/0, init/0, loop/1, start_child/1, stop_child/1, stop/0, get_names/0]).
 
 -type params() :: #{name => atom(), restart => permanent | temporary}.
 
+
+%% @doc Запускает сервер keylist_manager.
+-spec start() -> {ok, pid(), reference()}.
 start() ->
     ServerPid = spawn(?MODULE, init, []),
     register(?MODULE, ServerPid),
     MonitorRef = erlang:monitor(process, ServerPid),
     {ok, ServerPid, MonitorRef}.
 
+%% @doc Инициализирует сервер.
+-spec init() -> no_return().
 init() ->
     process_flag(trap_exit, true),
     loop(#state{}).
 
+%% @doc Основной цикл сервера.
+-spec loop(#state{}) -> no_return().
 loop(State) ->
     receive
         {From, start_child, #{name := Name, restart := Restart}} ->
@@ -57,18 +66,18 @@ loop(State) ->
             loop(State);
 
         {'EXIT', Pid, Reason} ->
-            case lists:keysearch(Pid, 2, State#state.children) of
+            case lists:keyfind(Pid, 2, State#state.children) of
                 {Name, Pid} ->
                     case lists:member(Pid, State#state.permanent) of
                         true -> 
                             io:format("Перезапуск процесса: ~p~n", [Pid]),
                             {ok, NewPid} = keylist:start_link(Name),
-                            NewChildren = lists:keyreplace(Name, 1, State#state.children, {Name, NewPid}),
+                            NewChildren = lists:keystore(Name, 1, State#state.children, {Name, NewPid}),
                             Newparm = lists:delete(Pid, State#state.permanent) ++ [NewPid],
                             loop(State#state{children = NewChildren, permanent = Newparm});
                         false ->
                             io:format("Завершение процесса: ~p, ~p~n", [Pid, Reason]),
-                            NewChildren = lists:keydelete(Pid, 2, State#state.children), % Вот честно не смог найти другой выход кроме как использовать функции lists :(
+                            NewChildren = lists:keydelete(Pid, 2, State#state.children), 
                             Newparm = lists:delete(Pid, State#state.permanent),
                             loop(State#state{children = NewChildren, permanent = Newparm})
                     end;
@@ -77,14 +86,26 @@ loop(State) ->
             end
     end.
 
+%% @doc Клиентская функция для запуска дочернего процесса.
+-spec start_child(params()) -> ok.
 start_child(Params) ->
-    keylist_manager ! {self(), start_child, Params}.
+    keylist_manager ! {self(), start_child, Params},
+    ok.
 
+%% @doc Клиентская функция для остановки дочернего процесса.
+-spec stop_child(atom()) -> ok.
 stop_child(Params) ->
-    keylist_manager ! {self(), stop_child, Params}.
+    keylist_manager ! {self(), stop_child, Params},
+    ok.
 
+%% @doc Клиентская функция для остановки сервера.
+-spec stop() -> ok.
 stop() ->
-    keylist_manager ! stop.
+    keylist_manager ! stop,
+    ok.
 
+%% @doc Клиентская функция для получения списка имен дочерних процессов.
+-spec get_names() -> ok.
 get_names() ->
-    keylist_manager ! {self(), get_state}.
+    keylist_manager ! {self(), get_state},
+    ok.
