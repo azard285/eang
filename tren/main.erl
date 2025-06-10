4d8ebe0a73f732a52dffd8eaf726ebfd52377dcb
azard285 <pavlenkopasa382@gmail.com>
2025-06-10 20:16:24 +0700

    add 2 tsk fibonachi

--- tren/main.erl
+++ tren/main.erl
@@ -1,5 +1,5 @@
 -module(main).
--export([fact/1]).
+-export([fact/1, fib/1]).
 
 fact(1) ->
     1;
@@ -7,6 +7,17 @@ fact(1) ->
 fact(N) ->
     N * fact(N-1).
 
+fib(1) ->
+    0;
 
-    
+fib(2) ->
+    1;  
+
+% fib(3) ->
+%     1;   
+
+fib(N) ->
+    fib(N-1) + fib(N-2).
+
+
 
