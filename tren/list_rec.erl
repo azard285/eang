a4c4479933ec1f4ebeb2a59f950632b6f02366bc
azard285 <pavlenkopasa382@gmail.com>
2025-06-10 21:46:52 +0700

    add 7tsk trut else filter

--- tren/list_rec.erl
+++ tren/list_rec.erl
@@ -1,5 +1,20 @@
 -module(list_rec).
--export([my_map/2]).
+-export([my_map/2, my_filter/2]).
 
 my_map(Fun, List) ->
     [Fun(I) || I <- List].
+
+%---------------------------------
+
+% my_fil_check(Fun, N) ->
+%     TF = Fun(N),
+%     if 
+%         TF ->
+%             N;
+%         true ->
+%             ok
+            
+%     end.
+
+my_filter(Fun, List) ->
+    [N || N <- List, Fun(N)].
