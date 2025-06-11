4aa8f6672c2f88768a49f882050e5e3aa9bfe98b
azard285 <pavlenkopasa382@gmail.com>
2025-06-11 22:04:25 +0700

    все оч плохо, 9 задание не дается, но появилась идея, погнали дальше

--- tren/list_rec.erl
+++ tren/list_rec.erl
@@ -1,5 +1,5 @@
 -module(list_rec).
--export([my_map/2, my_filter/2, fold/3]).
+-export([my_map/2, my_filter/2, fold/3, flat/1]).
 
 my_map(Fun, List) ->
     [Fun(I) || I <- List].
@@ -21,8 +21,37 @@ my_filter(Fun, List) ->
 
 %---------------------------------
 
-fold(Fun, [], Acc) ->
+fold(_Fun, [], Acc) ->
     Acc;
 
 fold(Fun, List, Acc) ->
     fold(Fun, tl(List), Fun(hd(List), Acc)).
+
+%---------------------------------
+
+% flat([]) ->
+%     [];
+
+% flat([H | T]) ->
+%     if 
+%         is_list(H) ->
+%             flat(H);
+%         true ->
+%             [H] ++ flat(T)
+%         end.
+% 
+
+
+flat_l(H) ->
+    if
+        is_list(H) ->
+            case H of
+                [] -> [];
+                [HE | TL] -> flat_l(HE) ++ flat_l(TL)
+            end;
+        true ->
+            H
+        end.
+
+flat(List) ->
+    [flat_l(N) || N <- List].
