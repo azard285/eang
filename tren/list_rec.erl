5462692bdf1f58502ab9f8af9a6d6c13c378f6ce
azard285 <pavlenkopasa382@gmail.com>
2025-06-11 21:21:40 +0700

    add 8tsk, release foldl

--- tren/list_rec.erl
+++ tren/list_rec.erl
@@ -1,5 +1,5 @@
 -module(list_rec).
--export([my_map/2, my_filter/2]).
+-export([my_map/2, my_filter/2, fold/3]).
 
 my_map(Fun, List) ->
     [Fun(I) || I <- List].
@@ -18,3 +18,11 @@ my_map(Fun, List) ->
 
 my_filter(Fun, List) ->
     [N || N <- List, Fun(N)].
+
+%---------------------------------
+
+fold(Fun, [], Acc) ->
+    Acc;
+
+fold(Fun, List, Acc) ->
+    fold(Fun, tl(List), Fun(hd(List), Acc)).
