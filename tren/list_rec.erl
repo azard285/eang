097eb5d8ffb2b673028f0e41de57618ced1f9705
azard285 <pavlenkopasa382@gmail.com>
2025-06-12 21:22:38 +0700

    easy tsk10 compl, remove_duplicate in list

--- tren/list_rec.erl
+++ tren/list_rec.erl
@@ -1,20 +1,10 @@
 -module(list_rec).
--export([my_map/2, my_filter/2, fold/3, flat/1]).
+-export([my_map/2, my_filter/2, fold/3, flat/1, rem_dup/1]).
 
 my_map(Fun, List) ->
     [Fun(I) || I <- List].
 
 %---------------------------------
-
-% my_fil_check(Fun, N) ->
-%     TF = Fun(N),
-%     if 
-%         TF ->
-%             N;
-%         true ->
-%             ok
-            
-%     end.
 
 my_filter(Fun, List) ->
     [N || N <- List, Fun(N)].
@@ -28,34 +18,6 @@ fold(Fun, List, Acc) ->
     fold(Fun, tl(List), Fun(hd(List), Acc)).
 
 %---------------------------------
-
-% flat([]) ->
-%     [];
-
-% flat([H | T]) ->
-%     if 
-%         is_list(H) ->
-%             flat(H);
-%         true ->
-%             [H] ++ flat(T)
-%         end.
-% 
-
-
-% flat_l(H) ->
-%     if
-%         is_list(H) ->
-%             case H of
-%                 [] -> [];
-%                 [HE | TL] -> flat_l(HE) ++ flat_l(TL)
-%             end;
-%         true ->
-%             H
-%         end.
-
-% flat(List) ->
-%     [flat_l(N) || N <- List].
-
 
 flat(List) ->
     flatt(List, []).
@@ -71,3 +33,18 @@ flatt([H | T], Acc) ->
 flatt([], Acc) ->
     Acc.
 
+%---------------------------------
+
+rem_dup(List) ->
+    rem_dup(List, []).
+
+rem_dup([H|T], Acc) ->
+    case lists:member(H, Acc) of
+        false ->
+            rem_dup(T, Acc ++ [H]);
+        true ->
+            rem_dup(T, Acc)
+        end;
+
+rem_dup([], Acc) ->
+    Acc.
