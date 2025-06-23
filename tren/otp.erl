01b7e2f5ef786918fa7f6fc50028cecffeec5a60
azard285 <pavlenkopasa382@gmail.com>
2025-06-23 19:23:08 +0700

    compl 21tsk, сервер очереди

--- tren/otp.erl
+++ tren/otp.erl
@@ -8,7 +8,57 @@
 
 % Работа с JSON: Используй библиотеку jsx для парсинга и генерации JSON.
 
--module(otp).
 
--export([]).
+
+-module(otp).
+-behaviour(gen_server).
+
+
+-export([start/0, init/0, pop/0, push/1, peek/0, handle_call/3, handle_cast/2]).
+
+%--------------------------------- 21 task
+% Что такое сервер очереди?
+% Это процесс, который:
+
+% Хранит элементы в порядке FIFO (First In, First Out)
+
+% Поддерживает операции:
+
+% push — добавить элемент в конец
+
+% pop — взять элемент из начала
+
+% peek — посмотреть первый элемент без удаления
+
+
+start() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+push(Atom) ->
+    gen_server:cast(?MODULE, {push, Atom}).
+
+pop() ->
+    gen_server:call(?MODULE, pop).
+
+peek() ->
+    gen_server:call(?MODULE, peek).
+
+
+init() ->
+    {ok, []}.
+
+handle_call(pop, _From, [H|T]) ->
+    {reply, H, T};
+handle_call(pop, _From, []) ->
+    {reply, empty, []};
+handle_call(peek, _From, [H|T]) ->
+    {reply, H, [H|T]};
+handle_call(peek, _From, []) ->
+    {reply, empty, []}.
+
+
+handle_cast({push, Atom}, State) ->
+    {noreply, State ++ [Atom]}.
+    
+
 
