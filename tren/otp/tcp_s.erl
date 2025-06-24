34008ebb6aaa4cca709a84331285ab205333c275
azard285 <pavlenkopasa382@gmail.com>
2025-06-24 17:44:24 +0700

    do simple tcp server for upstring, use telnet (сука я час сидел и у меня ничего не работало просто потому что active должен был быть false, это пипец).

---
+++ tren/otp/tcp_s.erl
@@ -0,0 +1,67 @@
+% TCP-сервер: Напиши простой TCP-сервер, который принимает строки и возвращает их в верхнем регистре.
+%--------------------------------- 23 task
+
+-module(tcp_s).
+
+-export([start/1, stop/0]).
+
+
+start(Port) ->
+    {ok, LSock} = gen_tcp:listen(Port, [binary, {active, once}, {reuseaddr, true}, {active, false}]),
+    io:format("Сервер запущен на порту ~p~n", [Port]),
+
+    ets:new(serv, [named_table, public]),
+    ets:insert(serv, {value, LSock}),
+
+    Pid = [spawn(fun() -> accept_loop(LSock) end)],
+    ets:insert(serv, {pids, Pid}),
+    {ok, LSock}.
+
+accept_loop(LSock) ->
+    case gen_tcp:accept(LSock) of
+        {ok, Socket} ->
+            io:format("Новое подключение~n"),
+
+            Pid = spawn(fun() -> handle(Socket) end),
+            gen_tcp:controlling_process(Socket, Pid),
+            ets:insert(serv, {pids, [Pid] ++ ets:lookup(serv, pids)}),
+
+            accept_loop(LSock);
+        {error, Reason} ->
+            io:format("Ошибка подключение: ~s~n", [Reason]),
+            accept_loop(LSock)
+    end.
+
+stop() ->
+    [exit(N, normal) || N <- ets:lookup(serv, pids)],
+    gen_tcp:close(ets:lookup(serv, value)).
+
+handle(Socket) ->
+    case gen_tcp:recv(Socket, 0) of
+        {ok, Data} ->
+            String = unicode:characters_to_list(Data),
+            UpString = verh_reg(String),
+            io:format("получил: ~s   отправляю: ~s~n", [String, UpString]),
+            
+            gen_tcp:send(Socket, UpString),
+            handle(Socket);
+
+        {error, closed} ->
+            io:format("Клиент закрыл соединение~n"),
+            gen_tcp:close(Socket);
+
+        {error, Reason} ->
+            io:format("Ошибка: ~p~n", [Reason]),
+            gen_tcp:close(Socket)
+    end.
+        
+check(N) ->
+    if 
+        (N < 97) or (N > 122) ->
+            false;
+        true -> 
+            true
+    end.
+
+verh_reg(Text) ->
+    [N - 32 || N <- Text, check(N)].
